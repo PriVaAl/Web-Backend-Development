11a257e3cd7f8a9bf17ceb9227036afe44db15e2
PriVaAl <priscilla.valverde@gmail.com>
2026-03-16 21:59:21 -0600

    Learning Activity 1

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const utilities = require("./utilities/")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * Static Files CSS Images
@@ -27,13 +30,31 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-
+app.use("/inv", inventoryRoute)
 /*************************
  * Index route
  */
-app.get("/", function(req, res){
-  res.render("index", {title:"Home"})
+app.get("/", baseController.buildHome)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
