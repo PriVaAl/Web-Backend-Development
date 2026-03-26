3d5cd586caa80eb3f30435ac58a2ae328ee43654
PriVaAl <priscilla.valverde@gmail.com>
2026-03-25 21:12:06 -0600

    Team Activity

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/")
+const regValidate = require('../utilities/account-validation')
 
 // Route to build account login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -10,8 +11,21 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build account registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-// Route to process registration
-router.post("/register", utilities.handleErrors(accountController.registerAccount));
-
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+// Process the login attempt
+router.post(
+  "/login",
+  regValidate.loginRules(),
+  regValidate.checkLoginData,
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
 
 module.exports = router;
