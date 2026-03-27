680b1a600ce2fc0cad6522653cd7531ee181e02d
PriVaAl <priscilla.valverde@gmail.com>
2026-03-26 20:29:41 -0600

    Assigment 4

--- controllers/invController.js
+++ controllers/invController.js
@@ -42,4 +42,102 @@ invCont.triggerError = utilities.handleErrors(async function(req, res, next) {
   throw new Error("Intentional 500 error triggered!")
 })
 
+/* ***************************
+ *  Build inventory management view
+ * ************************** */
+invCont.buildManagement = utilities.handleErrors(async function (req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("./inventory/management", {
+    title: "Inventory Management",
+    nav,
+    errors: null,
+  })
+})
+/* ***************************
+ *  Build add classification view
+ * ************************** */
+invCont.buildAddClassification = utilities.handleErrors(async function (req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("./inventory/add-classification", {
+    title: "Add Classification",
+    nav,
+    errors: null,
+  })
+})
+
+/* ***************************
+ *  Process add classification
+ * ************************** */
+invCont.addClassification = utilities.handleErrors(async function (req, res, next) {
+  const { classification_name } = req.body
+  const result = await invModel.insertClassification(classification_name)
+  if (result.rowCount) {
+    let nav = await utilities.getNav()
+    req.flash("notice", `Classification "${classification_name}" added successfully.`)
+    res.status(201).render("./inventory/management", {
+      title: "Inventory Management",
+      nav,
+      errors: null,
+    })
+  } else {
+    req.flash("notice", "Sorry, the classification could not be added.")
+    res.status(501).render("./inventory/add-classification", {
+      title: "Add Classification",
+      nav,
+      errors: null,
+    })
+  }
+})
+/* ***************************
+ *  Build add inventory view
+ * ************************** */
+invCont.buildAddInventory = utilities.handleErrors(async function (req, res, next) {
+  let nav = await utilities.getNav()
+  let classificationList = await utilities.buildClassificationList()
+  res.render("./inventory/add-inventory", {
+    title: "Add Inventory",
+    nav,
+    classificationList,
+    errors: null,
+  })
+})
+
+/* ***************************
+ *  Process add inventory
+ * ************************** */
+invCont.addInventory = utilities.handleErrors(async function (req, res, next) {
+  const {
+    inv_make, inv_model, inv_year, inv_description,
+    inv_image, inv_thumbnail, inv_price, inv_miles,
+    inv_color, classification_id
+  } = req.body
+  const result = await invModel.insertInventory(
+    inv_make, inv_model, inv_year, inv_description,
+    inv_image, inv_thumbnail, inv_price, inv_miles,
+    inv_color, classification_id
+  )
+  if (result.rowCount) {
+    let nav = await utilities.getNav()
+    req.flash("notice", `${inv_year} ${inv_make} ${inv_model} added successfully.`)
+    res.status(201).render("./inventory/management", {
+      title: "Inventory Management",
+      nav,
+      errors: null,
+    })
+  } else {
+    let nav = await utilities.getNav()
+    let classificationList = await utilities.buildClassificationList(classification_id)
+    req.flash("notice", "Sorry, the inventory item could not be added.")
+    res.status(501).render("./inventory/add-inventory", {
+      title: "Add Inventory",
+      nav,
+      classificationList,
+      errors: null,
+      inv_make, inv_model, inv_year, inv_description,
+      inv_image, inv_thumbnail, inv_price, inv_miles,
+      inv_color, classification_id
+    })
+  }
+})
+
 module.exports = invCont
--- models/inventory-model.js
+++ models/inventory-model.js
@@ -42,4 +42,39 @@ async function getInventoryById(inv_id) {
     console.error("getInventoryById error " + error)
   }
 }
-module.exports = {getClassifications, getInventoryByClassificationId, getInventoryById};
+
+/* ***************************
+ *  Add new classification
+ * ************************** */
+async function insertClassification(classification_name) {
+  try {
+    const sql = "INSERT INTO public.classification (classification_name) VALUES ($1) RETURNING *"
+    return await pool.query(sql, [classification_name])
+  } catch (error) {
+    return error.message
+  }
+}
+/* ***************************
+ *  Add new inventory item
+ * ************************** */
+async function insertInventory(
+  inv_make, inv_model, inv_year, inv_description,
+  inv_image, inv_thumbnail, inv_price, inv_miles,
+  inv_color, classification_id
+) {
+  try {
+    const sql = `INSERT INTO public.inventory 
+      (inv_make, inv_model, inv_year, inv_description, inv_image, 
+      inv_thumbnail, inv_price, inv_miles, inv_color, classification_id) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`
+    return await pool.query(sql, [
+      inv_make, inv_model, inv_year, inv_description,
+      inv_image, inv_thumbnail, inv_price, inv_miles,
+      inv_color, classification_id
+    ])
+  } catch (error) {
+    return error.message
+  }
+}
+
+module.exports = {getClassifications, getInventoryByClassificationId, getInventoryById, insertClassification, insertInventory};
